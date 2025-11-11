1e2f217bf4ca8add80cf8ae020fc03ac56208b5d
Yves Tanas <Yves.Tanas@gmx.de>
2025-11-11 08:23:23 +0100

    Reset destroyed copyright notices

--- Source/SpiderNavigation/Private/Behavior/Spider/Decorator/SpiderIsAtLocationDecorator.cpp
+++ Source/SpiderNavigation/Private/Behavior/Spider/Decorator/SpiderIsAtLocationDecorator.cpp
@@ -1,5 +1,25 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
+//The MIT License
+//
+//Copyright(C) 2017 Roman Nix
+//Copyright(C) 2025 Yves Tanas
+//
+//Permission is hereby granted, free of charge, to any person obtaining a copy
+//of this software and associated documentation files(the "Software"), to deal
+//in the Software without restriction, including without limitation the rights
+//to use, copy, modify, merge, publish, distribute, sublicense, and / or sell
+//copies of the Software, and to permit persons to whom the Software is
+//furnished to do so, subject to the following conditions :
+//
+//The above copyright notice and this permission notice shall be included in
+//all copies or substantial portions of the Software.
+//
+//THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE
+//AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//THE SOFTWARE.
 
 #include "Behavior/Spider/Decorator/SpiderIsAtLocationDecorator.h"
 
@@ -57,7 +77,7 @@ FString USpiderIsAtLocationDecorator::GetStaticDescription() const
 {
 	const FString AbortText = UBehaviorTreeHelpers::AbortModeToText(FlowAbortMode);
 
-	// Zeile nur anhängen, wenn sinnvoll
+	// Zeile nur anhï¿½ngen, wenn sinnvoll
 	const FString AbortLine = AbortText.IsEmpty() ? TEXT("") : (AbortText + TEXT("\n"));
 
 	return FString::Printf(
